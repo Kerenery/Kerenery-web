6a225e9b1419ca96dabba492da312cb214f1d6fb
Nicky Basis <djhitekdieant@gmail.com>
2022-11-15 22:42:00 +0300

    feat: manga page

--- journey-valley/scripts/manga-item.js
+++ journey-valley/scripts/manga-item.js
@@ -36,14 +36,95 @@ function loadData() {
 }
 
 
-function displayManga(data) {
+async function displayManga(data) {
     console.log(data);
     let manga_id = data.data.mal_id;
-    let manga_pictures = loadPictures(manga_id);
-    let manga_data = getFullMangaData(manga_id);
+    let manga_pictures = await loadPictures(manga_id);
+    let manga_data = await getFullMangaData(manga_id);
     console.log(manga_data);
     console.log(manga_id);
     console.log(manga_pictures);
+    let manga_main_pic = manga_data.images.jpg.large_image_url;
+    console.log(manga_main_pic);
+    const mangaItem = `
+        <div class="manga-item-inner">
+            <div class="manga-item-img">
+              <img src="${manga_main_pic}" alt=""/>
+            </div>
+            <div class="manga-item-info">
+              <h1>${manga_data.title}</h1>
+              <p>${manga_data.synopsis}</p>
+            </div>   
+        </div>
+
+        <div class="first-chapter">
+            <img src="${manga_pictures[0].jpg.image_url}">
+            <div class="first-chapter-info">
+                <h2>Chapter 1</h2>
+                <p>One Piece is a Japanese manga series,</p>
+                <div class="btn-list">
+                    <div class="first-chapter-btn">
+                        <a href="manga-page.html">Buy</a>
+                    </div>
+                    <div class="first-chapter-btn">
+                        <a href="manga-page.html">Read</a>
+                    </div>
+                    <div class="first-chapter-btn">
+                        <a href="manga-page.html">Something</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="all-chapters-list">
+            <!-- n chapters pictures -->
+            <div class="container">
+                <div class="grid-row">
+                    <div class="grid-item">
+                        <img class="pussy_image" src="assets/img/des1.png" />
+                        <img class="pussy_image" src="assets/img/des1.png" />
+                        <img class="pussy_image" src="assets/img/des1.png" />
+                    </div>
+                    <div class="grid-item">
+                        <img class="pussy_image" src="assets/img/des1.png" />
+                        <img class="pussy_image" src="assets/img/des1.png" />
+                        <img class="pussy_image" src="assets/img/des1.png" />
+                    </div>
+                    <div class="grid-item">
+                        <img class="pussy_image" src="assets/img/des1.png"" />
+                        <img class="pussy_image" src="assets/img/des1.png" />
+                        <img class="pussy_image" src="assets/img/des1.png" />
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="belt">
+            illustrated
+        </div>
+
+        <div class="manga-illustrations">
+            <!-- n illustrations pictures -->
+            <div class="container">
+                <div class="grid-row">
+                    <div class="grid-item">
+                        <img class="pussy_image" src="assets/img/des1.png" />
+                        <img class="pussy_image" src="assets/img/des1.png" />
+                        <img class="pussy_image" src="assets/img/des1.png" />
+                    </div>
+                    <div class="grid-item">
+                        <img class="pussy_image" src="assets/img/des1.png" />
+                        <img class="pussy_image" src="assets/img/des1.png" />
+                        <img class="pussy_image" src="assets/img/des1.png" />
+                    </div>
+                    <div class="grid-item">
+                        <img class="pussy_image" src="assets/img/des1.png"" />
+                        <img class="pussy_image" src="assets/img/des1.png" />
+                        <img class="pussy_image" src="assets/img/des1.png" />
+                    </div>
+                </div>
+            </div>`;
+            document.querySelector(".manga-item").insertAdjacentHTML("beforeend", mangaItem);
 }
 
 function pageLoaded() {
